a4e6f3bb48bbf381adbc4d575d20a47ed4f566e7
carlos-cb <carlos_chubei@hotmail.com>
2016-08-15 08:26:00 +0800

    前端准备工作完成

--- src/ChisnbalBundle/Controller/DefaultController.php
+++ src/ChisnbalBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace ChisnbalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ChisnbalBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('ChisnbalBundle:Default:index.html.twig');
+    }
+
+    public function categoryListAction()
+    {
+        return $this->render('ChisnbalBundle:Default:categoryList.html.twig');
+    }
+
+    public function productListAction()
+    {
+        return $this->render('ChisnbalBundle:Default:productList.html.twig');
+    }
+
+    public function productDetalleAction()
+    {
+        return $this->render('ChisnbalBundle:Default:productDetalle.html.twig');
     }
 }
