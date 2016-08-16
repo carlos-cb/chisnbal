1e98ce3f4a425a96e8514b7b401c020afd0470d5
carlos-cb <carlos_chubei@hotmail.com>
2016-08-17 06:48:39 +0800

    完成登录页面的开发

---
+++ src/ChisnbalBundle/Resources/public/js/login.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("a.various").fancybox({
+        beforeShow: function(){
+            $(".fancybox-skin").css("backgroundColor","transparent");
+        },
+        closeBtn		: false
+    });
+    $("div.login").click(function(){
+        var path = $(this).attr("data-path");
+        window.location.replace(path);
+    })
+});
