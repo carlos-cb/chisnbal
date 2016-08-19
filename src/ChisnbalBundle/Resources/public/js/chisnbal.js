e5a6fd8cd933ad31d8a6353f156a1e4336093cb1
carlos-cb <carlos_chubei@hotmail.com>
2016-08-19 20:17:07 +0800

    订单客户信息填写页面完成

--- src/ChisnbalBundle/Resources/public/js/chisnbal.js
+++ src/ChisnbalBundle/Resources/public/js/chisnbal.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 
     $(".fancybox").fancybox();
 
+    /* Apply fancybox to multiple items */
+
+    $("a.group").fancybox({
+        'transitionIn'	:	'elastic',
+        'transitionOut'	:	'elastic',
+        'speedIn'		:	600,
+        'speedOut'		:	200,
+        'overlayShow'	:	false
+    });
 });
