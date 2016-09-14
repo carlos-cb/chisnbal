d4367797a0483b31733a924c3f7f4d22ed8076a1
carlos-cb <carlos_chubei@hotmail.com>
2016-09-15 07:49:16 +0800

    输入用户信息结账页面 金额修改未完成

--- src/ChisnbalBundle/Resources/public/js/validation.js
+++ src/ChisnbalBundle/Resources/public/js/validation.js
@@ -24,21 +24,59 @@ function checkform(){
 }
 $(document).ready(function() {
     var qianinicial = parseFloat($("span#preciofinal").text());
+    var qianiva = qianinicial * 1.21;
+    $("span#preciofinaliva").html(qianiva.toFixed(2));
+    $("span#preciofinalzuizhong").html(qianiva.toFixed(2));
     var qian = 0;
+    var yunshufei = 0;
+    //读取运输方式
+    $("input[name='radio-group']").change(function () {
+        if($(this).val() == 1) {
+            yunshufei = 0;
+        }
+        if($(this).val() == 2) {
+            yunshufei = 10;
+        }
+        if($(this).val() == 3) {
+            yunshufei = 15;
+        }
+        var jj1 = $('select#paytype').children('option:selected').val();
+        if(jj1 == 3){
+            var jj2 = parseFloat($("span#preciofinaliva").text())*1.05 + yunshufei;
+        }else{
+            var jj2 = parseFloat($("span#preciofinaliva").text()) + yunshufei;
+        }
+        $("span#preciofinalzuizhong").html(jj2.toFixed(2));
+        setgerenshui()
+    });
+    //最终价格
     $('select#paytype').change(function () {
         var p1 = $(this).children('option:selected').val();//这就是selected的值
         if(p1 == 1)
         {
-            qian = qianinicial;
+            qian = qianiva + yunshufei;
         }
         if(p1 == 2)
         {
-            qian = qianinicial * 1.21;
+            qian = qianiva + yunshufei;
         }
         if(p1 == 3)
         {
-            qian = qianinicial * 1.05;
+            qian = qianiva*1.05 + yunshufei;
         }
-        $("span#preciofinal").html(qian.toFixed(2));
+        $("span#preciofinalzuizhong").html(qian.toFixed(2));
+        setgerenshui()
     });
+    $("input#gerenshui").change(function(){
+        setgerenshui();
+    });
+    setgerenshui();
+    function setgerenshui(){
+        var n = $("input#gerenshui:checked").length;
+        if(n){
+            var h = parseFloat($("span#preciofinalzuizhong").text())*1.052;
+            $("span#preciofinalzuizhong").html(h.toFixed(2));
+        }
+    }
+    
 });
